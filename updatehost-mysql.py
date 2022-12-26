83063d66cfe25955e42fcb9ebd3b4028ef4d6950
Rommel Salazar <rdsalazar@hotmail.com.com>
2022-12-26 14:24:51 -0600

    Test

--- updatehost-mysql.py
+++ updatehost-mysql.py
@@ -25,17 +25,17 @@ for data in datas:
 
 with open('/mnt/c/Users/rdsal/git_projects/mariadb-repl/master1_redhat.yaml','r') as f:
   content = f.read()
-  new_content=re.sub('ec2.*com',datas[1],content)
+  new_content=re.sub('ec2.*com',datas[0],content)
 with open('/mnt/c/Users/rdsal/git_projects/mariadb-repl/master1_redhat.yaml','w') as fo:
   fo.write(new_content)
 with open('/mnt/c/Users/rdsal/git_projects/mariadb-repl/master2_redhat.yaml','r') as f:
   content = f.read()
-  new_content=re.sub('ec2.*com',datas[3],content)
+  new_content=re.sub('ec2.*com',datas[2],content)
 with open('/mnt/c/Users/rdsal/git_projects/mariadb-repl/master2_redhat.yaml','w') as fo:
   fo.write(new_content)
 
 with open('/mnt/c/Users/rdsal/git_projects/mariadb-repl/slave_redhat.yaml','r') as f:
   content = f.read()
-  new_content=re.sub('ec2.*com',datas[5],content)
+  new_content=re.sub('ec2.*com',datas[4],content)
 with open('/mnt/c/Users/rdsal/git_projects/mariadb-repl/slave_redhat.yaml','w') as fo:
   fo.write(new_content)
